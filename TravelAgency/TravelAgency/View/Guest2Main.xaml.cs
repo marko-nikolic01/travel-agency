aedbd8da36cffb8dc5a70aa8d57026138ff3316a
IvanPartalo <ivanpartalo123@gmail.com>
2023-03-05 15:21:14 +0100

    Searching tours

--- TravelAgency/TravelAgency/View/Guest2Main.xaml.cs
+++ TravelAgency/TravelAgency/View/Guest2Main.xaml.cs
@@ -22,6 +22,7 @@ namespace TravelAgency.View
     public partial class Guest2Main : Window
     {
         public static ObservableCollection<TuraTest> Tours { get; set; }
+        List<TuraTest> toursList;
         public Guest2Main()
         {
             InitializeComponent();
@@ -32,6 +33,27 @@ namespace TravelAgency.View
                 new TuraTest("bg", 20),
                 new TuraTest("ns", 20)
             };
+            toursList = Tours.ToList();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            bool tbLocEmpty = true, tbDurEmpty = true;
+            if(tbLocation.Text != "" || tbDuration.Text != "")
+            {
+                if(tbLocation.Text != "")
+                    tbLocEmpty= false;
+                if (tbDuration.Text != "")
+                    tbDurEmpty = false;
+                //proverava da li tekst iz textbox zadovaljava kriterijum ili ako je prazan textbox onda svakako zadovoljava kriterijum
+                var filteredList = toursList.Where(x => (x.Location.ToLower().Contains(tbLocation.Text) || tbLocEmpty) &&
+                                                        (x.Duration.ToString().Contains(tbDuration.Text) || tbDurEmpty));
+                ToursDataGrid.ItemsSource = filteredList;
+            }
+            else
+            {
+                ToursDataGrid.ItemsSource = Tours;
+            }
         }
     }
 }
