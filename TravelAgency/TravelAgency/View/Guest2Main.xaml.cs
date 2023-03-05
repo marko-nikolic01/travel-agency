66956a4afde7a122314dae9fa6bf31b256614aef
IvanPartalo <ivanpartalo123@gmail.com>
2023-03-05 13:53:13 +0100

    Showing tours

--- TravelAgency/TravelAgency/View/Guest2Main.xaml.cs
+++ TravelAgency/TravelAgency/View/Guest2Main.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,6 +12,7 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using TravelAgency.Model;
 
 namespace TravelAgency.View
 {
@@ -19,9 +21,17 @@ namespace TravelAgency.View
     /// </summary>
     public partial class Guest2Main : Window
     {
+        public static ObservableCollection<TuraTest> Tours { get; set; }
         public Guest2Main()
         {
             InitializeComponent();
+            DataContext = this;
+
+            Tours = new ObservableCollection<TuraTest>
+            {
+                new TuraTest("bg", 20),
+                new TuraTest("ns", 20)
+            };
         }
     }
 }
