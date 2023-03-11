7417a0ab9feb4059e1c052244bdb5513c5a0d40e
Veljko121 <109314562+Veljko121@users.noreply.github.com>
2023-03-11 17:35:33 +0100

    Create IRepository.cs

---
+++ TravelAgency/TravelAgency/Repository/IRepository.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TravelAgency.Repository
+{
+    public interface IRepository<T>
+    {
+        List<T> GetAll();
+
+        T GetById(int id);
+
+        int NextId();
+
+        T Save(T entity);
+
+        void DeleteById(int id);
+
+        void Delete(T entity);
+
+        void DeleteAll();
+    }
+}
