cc1c35199cd983dfba5ea161cc5721c912b1fd02
marko-nikolic01 <116906156+marko-nikolic01@users.noreply.github.com>
2023-05-05 13:53:35 +0200

    komit

---
+++ TravelAgency/TravelAgency/WPF/ViewModels/AccommodationReservationViewModel.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media.Imaging;
+using TravelAgency.Domain.Models;
+using TravelAgency.Services;
+
+namespace TravelAgency.WPF.ViewModels
+{
+    public class AccommodationReservationViewModel : INotifyPropertyChanged
+    {
+        private AccommodationReservationService _reservationService;
+        private ReservationDateFinderService _dateFinderService;
+
+        private User _guest;
+        private Accommodation _accommodation;
+        private AccommodationReservation _reservation;
+        private int _dayNumber;
+        private DateTime _firstDate;
+        private DateTime _lastDate;
+        private ObservableCollection<DateSpan> _availableDateSpans;
+        private DateSpan _selectedDateSpan;
+        private List<BitmapImage> _photos;
+        private int _currentPhotoIndex;
+        private bool _shouldValidate;
+
+        public Accommodation Accommodation
+        {
+            get => _accommodation;
+            set
+            {
+                if (value != _accommodation)
+                {
+                    _accommodation = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+        public AccommodationReservation Reservation
+        {
+            get => _reservation;
+            set
+            {
+                if (value != _reservation)
+                {
+                    _reservation = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+        public int DayNumber
+        {
+            get => _dayNumber;
+            set
+            {
+                if (value != _dayNumber)
+                {
+                    _dayNumber = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+        public DateTime FirstDate
+        {
+            get => _firstDate;
+            set
+            {
+                if (value != _firstDate)
+                {
+                    _firstDate = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+        public DateTime LastDate
+        {
+            get => _lastDate;
+            set
+            {
+                if (value != _lastDate)
+                {
+                    _lastDate = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+        public ObservableCollection<DateSpan> AvailableDateSpans
+        {
+            get => _availableDateSpans;
+            set
+            {
+                if (value != _availableDateSpans)
+                {
+                    _availableDateSpans = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+        public DateSpan SelectedDateSpan
+        {
+            get => _selectedDateSpan;
+            set
+            {
+                if (value != _selectedDateSpan)
+                {
+                    _selectedDateSpan = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+        public AccommodationReservationViewModel()
+        {
+            _reservationService = new AccommodationReservationService();
+            _dateFinderService = new ReservationDateFinderService();
+
+            //InitializeData();
+        }
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
---
+++ TravelAgency/TravelAgency/WPF/Views/ForumsMenuView.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace TravelAgency.WPF.Views
+{
+    /// <summary>
+    /// Interaction logic for ForumsMenuView.xaml
+    /// </summary>
+    public partial class ForumsMenuView : Page
+    {
+        private Guest1HomeView _mainWindow;
+
+        public ForumsMenuView(Guest1HomeView guest1HomeView)
+        {
+            InitializeComponent();
+            _mainWindow = guest1HomeView;
+        }
+
+        private void ButtonBack_Click(object sender, RoutedEventArgs e)
+        {
+            _mainWindow.HighlightSelectedTab(_mainWindow.buttonHome);
+            this.NavigationService.Navigate(new HomeMenuView(_mainWindow));
+        }
+    }
+}
