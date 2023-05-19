84693bf55ba8e3841d784f49ec2b43cdbcea5074
Veljko121 <109314562+Veljko121@users.noreply.github.com>
2023-05-19 13:32:11 +0200

    Added statistics viewmodel

---
+++ TravelAgency/TravelAgency/WPF/ViewModels/OwnerAccommodationsStatisticsViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TravelAgency.Domain.Models;
+
+namespace TravelAgency.WPF.ViewModels
+{
+    public class OwnerAccommodationsStatisticsViewModel
+    {
+        public List<Accommodation> Accommodations { get; set; }
+    }
+}
