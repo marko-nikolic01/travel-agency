5a7e9242a15b8d9fb1a5006839a7871fad734cca
Veljko121 <109314562+Veljko121@users.noreply.github.com>
2023-05-19 13:38:38 +0200

    Update OwnerAccommodationsStatisticsViewModel.cs

--- TravelAgency/TravelAgency/WPF/ViewModels/OwnerAccommodationsStatisticsViewModel.cs
+++ TravelAgency/TravelAgency/WPF/ViewModels/OwnerAccommodationsStatisticsViewModel.cs
@@ -4,11 +4,27 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using TravelAgency.Domain.Models;
+using TravelAgency.Services;
 
 namespace TravelAgency.WPF.ViewModels
 {
     public class OwnerAccommodationsStatisticsViewModel
     {
+        private UserService userService;
+        private AccommodationService accommodationService;
+
+        private User loggedInUser;
+
         public List<Accommodation> Accommodations { get; set; }
+
+        public OwnerAccommodationsStatisticsViewModel()
+        {
+            userService = new UserService();
+            accommodationService = new AccommodationService();
+
+            loggedInUser = userService.GetLoggedInUser();
+
+            Accommodations = accommodationService.GetByOwner(loggedInUser);
+        }
     }
 }
