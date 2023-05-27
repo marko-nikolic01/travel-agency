d1fea2fe129134d2e7783d137f1e3b8677adbc34
marko-nikolic01 <116906156+marko-nikolic01@users.noreply.github.com>
2023-05-27 17:54:49 +0200

    Added Comment class

--- TravelAgency/TravelAgency/Domain/Models/Comment.cs
+++ TravelAgency/TravelAgency/Domain/Models/Comment.cs
@@ -5,40 +5,90 @@ using System.Linq;
 using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Linq;
 using TravelAgency.Serializer;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.ToolTip;
 
 namespace TravelAgency.Domain.Models
 {
     public class Comment : ISerializable, INotifyPropertyChanged
     {
-       
+        public int Id { get; set; }
+        public Forum Forum { get; set; }
+        public User User { get; set; }
+        private string _text;
+        private bool _locationVisited;
+        private bool _ownsAccommodationOnLocation;
+
+        public string Text
+        {
+            get => _text;
+            set
+            {
+                if (value != _text)
+                {
+                    _text = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+        public bool LocationVisited
+        {
+            get => _locationVisited;
+            set
+            {
+                if (value != _locationVisited)
+                {
+                    _locationVisited = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+        public bool OwnsAccommodationOnLocation
+        {
+            get => _ownsAccommodationOnLocation;
+            set
+            {
+                if (value != _ownsAccommodationOnLocation)
+                {
+                    _ownsAccommodationOnLocation = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+        public Comment() 
+        {
+            Forum = new Forum();
+            User = new User();
+            LocationVisited = false;
+            OwnsAccommodationOnLocation = false;
+        }
 
         public string[] ToCSV()
         {
             string[] csvValues =
-            {/*
+            {
                 Id.ToString(),
-                Name,
-                OwnerId.ToString(),
-                LocationId.ToString(),
-                Convert.ToInt32(Type).ToString(),
-                MaxGuests.ToString(),
-                MinDays.ToString(),
-                DaysToCancel.ToString()*/
+                Forum.Id.ToString(),
+                User.Id.ToString(),
+                Text,
+                Convert.ToInt32(LocationVisited).ToString(),
+                Convert.ToInt32(LocationVisited).ToString()
             };
             return csvValues;
         }
 
         public void FromCSV(string[] values)
-        {/*
+        {
             Id = Convert.ToInt32(values[0]);
-            Name = values[1];
-            OwnerId = int.Parse(values[2]);
-            LocationId = int.Parse(values[3]);
-            Type = (AccommodationType)Convert.ToInt32(values[4]);
-            MaxGuests = Convert.ToInt32(values[5]);
-            MinDays = Convert.ToInt32(values[6]);
-            DaysToCancel = Convert.ToInt32(values[7]);*/
+            Forum.Id = Convert.ToInt32(values[1]);
+            User.Id = Convert.ToInt32(values[2]);
+            Text = values[3];
+            LocationVisited = Convert.ToBoolean(Convert.ToInt32(values[4]));
+            OwnsAccommodationOnLocation = Convert.ToBoolean(Convert.ToInt32(values[5]));
         }
 
         public event PropertyChangedEventHandler? PropertyChanged;
