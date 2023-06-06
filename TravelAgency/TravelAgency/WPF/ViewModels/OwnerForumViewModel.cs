c990ed38b4033778dbc197080d56394ecbb46aca
Veljko121 <109314562+Veljko121@users.noreply.github.com>
2023-06-06 17:01:16 +0200

    Started forums

---
+++ TravelAgency/TravelAgency/WPF/ViewModels/OwnerForumViewModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TravelAgency.Domain.Models;
+using TravelAgency.Services;
+
+namespace TravelAgency.WPF.ViewModels
+{
+    public class OwnerForumViewModel : ViewModelBase
+    {
+        private User loggedInUser;
+
+        private UserService userService;
+        private ForumService forumService;
+
+        public OwnerForumViewModel()
+        {
+            userService = new UserService();
+            forumService = new ForumService();
+
+            loggedInUser = userService.GetLoggedInUser();
+        }
+    }
+}
